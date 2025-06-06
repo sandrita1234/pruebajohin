35c49665082a633dcad3827ce9aa0a1ca749ee88
sandra llorente <sandra@gmail.com>
2025-06-06 10:06:02 +0200

    commit inicial

---
+++ nuevaCalculadora/src/main/java/nuevaCalculadora/servicioPersona.java
@@ -0,0 +1,14 @@
+package nuevaCalculadora;
+
+import java.util.ArrayList;
+
+public class servicioPersona {
+	
+	ArrayList<persona> personas;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
